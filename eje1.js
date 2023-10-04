8effef200a9a16ba6f89306a5aa23abd660cd258
brayanAcosta <brayanacosta364@gmail.com>
2023-10-04 09:24:42 -0500

    EjeUnoTema3

--- eje1.js
+++ eje1.js
@@ -1,3 +1,4 @@
+// brayan estein acosta achinte cc 1107509939
 //Dada la calificación de un alumno mostrar en pantalla si es aprobado 
 //siempre y cuando tenga el 80% de sus asistencias, en caso de tener menos 
 //del 80% de asistencias se mostrará reprobado aún cuando su calificación 
